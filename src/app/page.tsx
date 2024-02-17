4942dabe98e2fcd7ee499588020c5a372502b74e
chukskull <thuglifevideo7@gmail.com>
2024-02-17 14:47:19 +0000

    adding a procedure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import { SignInButton, SignOutButton, auth } from "@clerk/nextjs";
 
+import { api } from "~/trpc/server";
+
 export default async function Home() {
   const { userId } = auth();
+  const { data } = await api.post.getAll.query();
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div>
